82ab8d0418b5d94e6d4a113f28bb03865ab58455
“tfogh” <2432141862@qq.com>
2024-06-24 00:24:36 +0800

    6.24

---
+++ Assets/PlayerLocomotion.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+namespace SG
+{
+    public class PlayerLocomotion : MonoBehaviour
+    {
+
+        Transform cameraObject;
+        InputHandler inputHandler;
+        Vector3 moveDirection;
+
+        [HideInInspector]
+        public Transform myTransform;
+
+        public new Rigidbody rigibody;
+        public GameObject normalCamera;
+        [Header("Stats")]
+        [SerializeField]
+        float movementSpeed = 5;
+        [SerializeField]
+        float rotationSpeed = 10;
+
+        void Start()
+        {
+            rigibody = GetComponent<Rigidbody>();
+            inputHandler = GetComponent<InputHandler>();
+            cameraObject = Camera.main.transform;
+            myTransform = transform;
+        }
+
+        #region Movement
+        Vector3 normalVector;
+        Vector3 TargetPosition;
+
+        private void HandleRotation(float delta)
+        {
+            Vector3 targetDir = Vector3.zero;
+            float moveOrride = inputHandler.moveAmount;
+
+            targetDir = cameraObject.forward * inputHandler.vertical;
+            targetDir += cameraObject.right * inputHandler.horizontal;
+
+            targetDir.Normalize();
+            targetDir.y = 0;
+
+            if (targetDir == Vector3.zero)
+            {
+                targetDir=myTransform.forward;
+            }
+
+            float rs = rotationSpeed;
+            Quaternion tr= Quaternion.LookRotation(targetDir);
+            Quaternion targetRotation= Quaternion.Slerp(myTransform.rotation, tr, rs*delta);
+
+            myTransform.rotation = targetRotation;
+        }
+
+        public void Update()
+        {
+            float delta = Time.deltaTime;
+
+            inputHandler.TickInput(delta);
+
+            moveDirection=cameraObject.forward*inputHandler.vertical;
+            moveDirection += cameraObject.right * inputHandler.horizontal;
+            moveDirection.Normalize();
+
+            float speed = movementSpeed;
+            moveDirection *= speed;
+
+            Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+            rigibody.velocity = projectedVelocity;
+
+
+        }
+        #endregion
+
+    }
+}
+
